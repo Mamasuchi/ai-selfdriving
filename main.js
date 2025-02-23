a7fa311d325ae251d676672513f0b32f24ae6ca5
Jose <treesjose22@gmail.com>
2025-02-23 13:50:58 -0500

    commit

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 const canvas=document.getElementById("myCanvas");
 canvas.height=window.innerHeight;
 canvas.width=200;
+
+const animation= canvas.getContext("2d");
+const car= new Car(100,100,35,55);
+car.draw(animation);
